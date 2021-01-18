7d1419162825f02113baa8d4df46810933b8ccb1
KingArthur-IT <67517973+KingArthur-IT@users.noreply.github.com>
2021-01-18 09:38:39 +0200

    Добавлены скрипты

---
+++ js/main.js
@@ -0,0 +1,37 @@
+//Закрепление меню
+$(window).scroll(function(){
+    var docscroll = $(document).scrollTop();
+    if(docscroll > $(window).height())
+    {
+        $('.main-menu').addClass('fixed-menu');
+        $('.main-menu li').addClass('fixed-menu__item');
+    }
+    else
+    {
+        $('.main-menu').removeClass('fixed-menu');
+        $('.main-menu li').removeClass('fixed-menu__item');
+    }
+});
+
+//Создание модального окна
+$(document).ready(function(){
+    $(".modalbox").fancybox(); //Показать модальное окно
+    $(".feedback-box__form").submit(function(){ return false; });
+    $(".feedback-box__form__btn").on("click", function(){         
+        // действия по обработке формы
+        $(".feedback-box__form").fadeOut("fast", function(){
+            $(this).before("<p><strong>Нажаль ця опція ще тестується!</strong></p>");
+            setTimeout("$.fancybox.close()", 5000);
+        });
+    });
+});
+
+//Заглушки на ссылки
+$(document).ready(function(){
+    $('.link-lockup').on('click', function(){
+        Swal.fire({
+            title: 'Це посилання поки не активне.',
+            icon: 'success'
+          })
+    });
+});
